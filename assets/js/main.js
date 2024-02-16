6646a670a6c9518422870b5fc3b978c0b89fa55e
Esteban Cáceres <estebancacerest@gmail.com>
2024-02-16 15:37:42 -0300

    :tada:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,82 @@
 $(function () {
-  // chart Canvajs
-  window.onload = function () {
-    var options = {
-      title: {
-        text: "Desktop OS Market Share in 2017",
-      },
-      subtitles: [
-        {
-          text: "As of November, 2017",
-        },
-      ],
-      animationEnabled: true,
-      data: [
-        {
-          type: "pie",
-          startAngle: 40,
-          toolTipContent: "<b>{label}</b>: {y}%",
-          showInLegend: "true",
-          legendText: "{label}",
-          indexLabelFontSize: 16,
-          indexLabel: "{label} - {y}%",
-          dataPoints: [
-            { y: 48.36, label: "Windows 7" },
-            { y: 26.85, label: "Windows 10" },
-            { y: 1.49, label: "Windows 8" },
-            { y: 6.98, label: "Windows XP" },
-            { y: 6.53, label: "Windows 8.1" },
-            { y: 2.45, label: "Linux" },
-            { y: 3.32, label: "Mac OS X 10.12" },
-            { y: 4.03, label: "Others" },
-          ],
-        },
-      ],
-    };
-    $("#chartContainer").CanvasJSChart(options);
 
-  };
+    //validar
+$("#formBusqueda").submit(function () {
+  event.preventDefault();
+
+  let numeroHeroe = $("#numeroSuperhero").val();
+  let regex = /^[1-9][0-9]{0,2}$/; // Regex para números entre 1 y 999
+
+  if (!regex.test(numeroHeroe) || numeroHeroe < 1 || numeroHeroe > 732) {
+    if (isNaN(numeroHeroe)) {
+      alert("Por favor, solo un número entre 1 y 732. No letras, no intentes buscar tu superheroe por su nombre :)");
+    } else {
+      alert("Por favor, ingresa un número entre 1 y 732.");
+    }
+    return;
+  }
+
+  buscarSuperHeroe(numeroHeroe);
 });
+
+});
+
+//fx para mostrar estadisticas en grafico
+function mostrarEstadisticas(data) {
+  let graficoPastel = new CanvasJS.Chart("chartContainer", {
+    theme: "dark1",
+    data: [
+      {
+        type: "pie",
+        showInLegend: true,
+        legendText: "{label}", // se agrega el label para mostrar como indicadores debajo del grafico
+        toolTipContent: "{label}: <strong>{y}%</strong>",
+        indexLabel: "{label} - {y}%",
+        dataPoints: [
+          { y: +data.powerstats.intelligence, label: "Inteligencia" },
+          { y: +data.powerstats.strength, label: "Fuerza" },
+          { y: +data.powerstats.speed, label: "Velocidad" },
+          { y: +data.powerstats.durability, label: "Durabilidad" },
+          { y: +data.powerstats.power, label: "Poder" },
+          { y: +data.powerstats.combat, label: "Combate" },
+        ],
+      },
+    ],
+  });
+  graficoPastel.render();
+}
+
+function buscarSuperHeroe(numeroHeroe) {
+  $.ajax({
+    url: "https://www.superheroapi.com/api.php/3525635500807579/" + numeroHeroe,
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      console.log(data);
+      // fx apis
+      $("#pfp").attr("src", data.image.url);
+      $("#name").text("Nombre: " + data.name);
+      $("#publicadoPor").text("Publicado por: " + data.biography.publisher);
+      $("#ocupacion").text("Ocupacion: " + data.work.occupation);
+      $("#primeraAparicion").text(
+        "Primera aparicion: " + data.biography["first-appearance"]
+      );
+      $("#lugarDeNacimiento").text(
+        "Lugar de Nacimiento: " + data.biography["place-of-birth"]
+      );
+      $("#altura").text("Altura: " + data.appearance.height.join(" | "));
+      $("#peso").text("Peso: " + data.appearance.weight.join(" | "));
+      $("#alianzas").text("Alianzas: " + data.connections["group-affiliation"]);
+      $("#parientes").text("Parientes: " + data.connections["relatives"]);
+
+      $("#tituloCard").text("Has seleccinado a: " + data.name);
+      $("#tituloGrafico").text("Estadisticas de: " + data.name);
+
+      $("#superHeroEncontrado").show();
+      mostrarEstadisticas(data);
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+}
