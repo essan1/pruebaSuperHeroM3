87012fa1c1fb606dcb32c6064d98c87b50523d9e
Esteban Cáceres <estebancacerest@gmail.com>
2024-02-19 01:39:49 -0300

    final desafio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 $(function () {
 
   //validar
-  $("#formBusqueda").submit(function (e) {
-    e.preventDefault();
+  $("#formBusqueda").submit(function (x) {
+    x.preventDefault();
 
     let numeroHeroe = $("#numeroSuperhero").val();
     let regex = /^[1-9][0-9]{0,2}$/; // regex entre 1 y 999, 
 
     if (!regex.test(numeroHeroe) || numeroHeroe < 1 || numeroHeroe > 732) {
-      if (isNaN(numeroHeroe)) {
+      if (isNaN(numeroHeroe))  { //not a number, error.
         // alert("Por favor, solo un número entre 1 y 732. No letras, no intentes buscar tu superheroe por su nombre :)");
         Swal.fire({
           icon: "error",
@@ -30,37 +30,7 @@ $(function () {
   });
 });
 
-
-//fx para mostrar estadisticas en grafico pastel
-function mostrarEstadisticas(data) {
-  let graficoPastel = new CanvasJS.Chart("chartContainer", {
-    theme: "dark1",
-    data: [
-      {
-        type: "pie",
-        showInLegend: true,
-        legendText: "{label}", // se agrega el label para mostrar como indicadores debajo del grafico
-        toolTipContent: "{label}: <strong>{y}%</strong>",
-        indexLabel: "{label} - {y}%",
-        //agrego aca los dataPoints de Y.
-        dataPoints: [
-          { y: +data.powerstats.intelligence, label: "Inteligencia" },
-          { y: +data.powerstats.strength, label: "Fuerza" },
-          { y: +data.powerstats.speed, label: "Velocidad" },
-          { y: +data.powerstats.durability, label: "Durabilidad" },
-          { y: +data.powerstats.power, label: "Poder" },
-          { y: +data.powerstats.combat, label: "Combate" },
-        ],
-      },
-    ],
-  });
-  graficoPastel.render();
-}
-
-
-
-
-//nos conectamos a la api x ajax
+//conexion a la api atraves de ajax + agregar info a la card
 function buscarSuperHeroe(numeroHeroe) {
   $.ajax({
     url: "https://www.superheroapi.com/api.php/3525635500807579/" + numeroHeroe,
@@ -96,3 +66,35 @@ function buscarSuperHeroe(numeroHeroe) {
     },
   });
 }
+
+
+
+//fx para mostrar estadisticas en grafico pastel
+function mostrarEstadisticas(data) {
+  let graficoPastel = new CanvasJS.Chart("chartContainer", {
+    theme: "dark1",
+    data: [
+      {
+        type: "pie",
+        showInLegend: true,
+        legendText: "{label}",
+        toolTipContent: "{label}: <strong>{y}%</strong>",
+        indexLabel: "{label} - {y}%",
+        //se agregan aca los dataPoints de Y.
+        dataPoints: [
+          { y: +data.powerstats.intelligence, label: "Inteligencia" },
+          { y: +data.powerstats.strength, label: "Fuerza" },
+          { y: +data.powerstats.speed, label: "Velocidad" },
+          { y: +data.powerstats.durability, label: "Durabilidad" },
+          { y: +data.powerstats.power, label: "Poder" },
+          { y: +data.powerstats.combat, label: "Combate" },
+        ],
+      },
+    ],
+  });
+  graficoPastel.render();
+}
+
+
+
+
